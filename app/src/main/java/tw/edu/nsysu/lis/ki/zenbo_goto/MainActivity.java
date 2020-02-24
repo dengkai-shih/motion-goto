1c5a11b7ecc083de57ff392fe3a4a07d1fdc3ba3
dengkai <pinkuang@mail.nsysu.edu.tw>
2020-02-24 17:27:51 +0800

    Zenbo Map Funtion Sample

---
+++ app/src/main/java/tw/edu/nsysu/lis/ki/zenbo_goto/MainActivity.java
@@ -0,0 +1,160 @@
+package tw.edu.nsysu.lis.ki.zenbo_goto;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.asus.robotframework.API.RobotCallback;
+import com.asus.robotframework.API.RobotCmdState;
+import com.asus.robotframework.API.RobotErrorCode;
+import com.asus.robotframework.API.RobotFace;
+import com.robot.asus.robotactivity.RobotActivity;
+
+import org.json.JSONObject;
+
+public class MainActivity extends RobotActivity {
+    // request code for READ_CONTACTS. It can be any number > 0.
+    private static final int PERMISSIONS_REQUEST_READ_CONTACTS = 100;
+
+    // robotAPI flags
+    private static boolean isRobotApiInitialed = false;
+
+    // 1st roomInfo string
+    private String sFirstRoom;
+
+    // buttons
+    private Button mButtonGrantPermission;
+    private Button mButtonGetRoomInfo;
+    private Button mButtonGoTo;
+
+    // textViews
+    private TextView mTextViewPermissionStatus;
+    private TextView mTextViewFirstRoomKeyword;
+
+    public static RobotCallback robotCallback = new RobotCallback() {
+        @Override
+        public void onResult(int cmd, int serial, RobotErrorCode err_code, Bundle result) {
+            super.onResult(cmd, serial, err_code, result);
+        }
+
+        @Override
+        public void onStateChange(int cmd, int serial, RobotErrorCode err_code, RobotCmdState state) {
+            super.onStateChange(cmd, serial, err_code, state);
+        }
+
+        @Override
+        public void initComplete() {
+            super.initComplete();
+
+            Log.d("ZenboGoToLocation", "initComplete()");
+            isRobotApiInitialed = true;
+        }
+    };
+
+    public static RobotCallback.Listen robotListenCallback = new RobotCallback.Listen() {
+        @Override
+        public void onFinishRegister() {
+
+        }
+
+        @Override
+        public void onVoiceDetect(JSONObject jsonObject) {
+
+        }
+
+        @Override
+        public void onSpeakComplete(String s, String s1) {
+
+        }
+
+        @Override
+        public void onEventUserUtterance(JSONObject jsonObject) {
+
+        }
+
+        @Override
+        public void onResult(JSONObject jsonObject) {
+
+        }
+
+        @Override
+        public void onRetry(JSONObject jsonObject) {
+
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        robotAPI.robot.setExpression(RobotFace.HIDEFACE);
+
+        // textViews
+        mTextViewPermissionStatus = (TextView) findViewById(R.id.textView_permission_status);
+        mTextViewFirstRoomKeyword = (TextView) findViewById(R.id.textView_roomInfo_1st_keyword);
+
+
+        // buttons
+        mButtonGrantPermission = (Button) findViewById(R.id.button_requestPermission);
+        mButtonGrantPermission.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // request READ_CONTACTS permission
+                requestPermission();
+
+            }
+        });
+    }
+
+    public MainActivity() {
+        super(robotCallback, robotListenCallback);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // robotAPI.robot.speak("Hello world. I am Zenbo. Nice to meet you.");
+    }
+
+    private void requestPermission() {
+        // Check the SDK version and whether the permission is already granted or not.
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M ||
+                this.checkSelfPermission(Manifest.permission.READ_CONTACTS) ==
+                        PackageManager.PERMISSION_GRANTED) {
+            // Android version is lesser than 6.0 or the permission is already granted.
+            Log.d("ZenboGoToLocation", "permission is already granted");
+            return;
+        }
+
+        if (shouldShowRequestPermissionRationale(Manifest.permission.READ_CONTACTS)) {
+            requestPermissions(new String[]{Manifest.permission.READ_CONTACTS},
+                    PERMISSIONS_REQUEST_READ_CONTACTS);
+        } else {
+            //showMessageOKCancel("You need to allow access to Contacts",
+            //        new DialogInterface.OnClickListener() {
+            //            @Override
+            //            public void onClick(DialogInterface dialog, int which) {
+            requestPermissions(new String[]{Manifest.permission.READ_CONTACTS},
+                    PERMISSIONS_REQUEST_READ_CONTACTS);
+            //            }
+            //        });
+        }
+    }
+
+    /*
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new android.support.v7.app.AlertDialog.Builder(this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
+    }
+    */
+}
